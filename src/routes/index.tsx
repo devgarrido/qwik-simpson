c717ba4fc1b9c6ccd731e8a8897936e9e130f347
desarrollo-garrido <desarrollo@garrido.pro>
2023-05-22 11:15:15 +0200

    🆕 feat: first commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,20 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
     <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
+      <h1 class="text-red-800 bg-brand">The Simpsons App</h1>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: "The Simpsons App",
   meta: [
     {
-      name: 'description',
-      content: 'Qwik site description',
+      name: "description",
+      content: "Página de inicio de la app The Simpsons Quote",
     },
   ],
 };
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#ffde00",
+        contrast: "#ee3133",
+        light: "#fff",
+      },
+    },
+  },
+  plugins: [],
+};
